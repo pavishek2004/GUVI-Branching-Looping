0bb8cb38c516a7240eb3947011235ea303ec2f98
pavishek2004 <141042084+pavishek2004@users.noreply.github.com>
2025-04-12 21:53:44 +0530

    Add files via upload

---
+++ PrintNumbers.java
@@ -0,0 +1,10 @@
+package guvi.assignment;
+
+public class PrintNumbers {
+	
+	    public static void main(String[] args) {
+	        for (int i = 10; i <= 50; i++) {
+	            System.out.println(i);
+	        }
+	    }
+	}
